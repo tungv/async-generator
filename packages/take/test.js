2f86c1567ec711955d5533fd4e20c75c64b3969f
Tung <tung@bodidata.com>
2019-10-03 14:07:14 +0700

    test(take): add short-circuit test

--- packages/take/test.js
+++ packages/take/test.js
@@ -13,3 +13,21 @@ it("should take n items and stop", async () => {
 
   expect(actual).toEqual([0, 1, 2, 3, 4]);
 });
+
+it("should end if generator ends", async () => {
+  async function* input() {
+    yield 1;
+    yield 2;
+    yield 3;
+    return 4;
+  }
+
+  const output = take(input(), 10);
+  const actual = [];
+
+  for await (const i of output) {
+    actual.push(i);
+  }
+
+  expect(actual).toEqual([1, 2, 3, 4]);
+});
